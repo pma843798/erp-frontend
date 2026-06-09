caf5badb18b6945944e98123342962a375c88a0a
Nikhil <nikhil74280171@gmail.com>
2026-06-09 18:00:50 +0530

    main file

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import 'primereact/resources/themes/lara-light-blue/theme.css'; 
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
